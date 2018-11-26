1609eaa2f9fc65ee5ac2f3d4a215c160365c61bf
Asmadouce <asmadouce@hotmail.fr>
2018-11-26 10:05:55 +0100

    ajout modif

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="library/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" href="library/fontawesome/css/all.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
     <title>Document</title>
 </head>
 <body>
@@ -14,5 +17,8 @@
 </head>
 <body>
 	<h1>Hello World!</h1>
+
+<script src="script/jQuery.js"></script>
+<script src="library/bootstrap/js/bootstrap.bundle.js"></script> 
 </body>
 </html>
