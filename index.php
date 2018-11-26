1a21679e437186a89ab77be2fe10fd82e7791af0
Asmadouce <asmadouce@hotmail.fr>
2018-11-26 10:15:33 +0100

    maj

--- index.php
+++ index.php
@@ -3,41 +3,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
     <link rel="stylesheet" href="library/bootstrap/css/bootstrap.css" />
 	<link rel="stylesheet" href="library/fontawesome/css/all.css" />
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
-    <title>Document</title>
-=======
     <title>Explorateur de fichiers</title>
->>>>>>> 5b00e3d5e5f2586e314edb1337e47f634d4dfb78
 </head>
 <body>
-
     <h1>Asmadouce et khris87 font un explorateur de fichiers</h1>
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Hello World!</h1>
+		<section id="explorateur">
+		    <div class="container">
+		        <button><i class="fas fa-angle-left"></i></button>
+		        <input type="text">
+		        <div id="champ_dossier">
 
-<script src="script/jQuery.js"></script>
-<script src="library/bootstrap/js/bootstrap.bundle.js"></script> 
-=======
-
-<section id="explorateur">
-    <div class="container">
-        <button><i class="fas fa-angle-left"></i></button>
-        <input type="text">
-        <div id="champ_dossier">
-
-        </div>
-    </div>
-</section>
-
->>>>>>> 5b00e3d5e5f2586e314edb1337e47f634d4dfb78
+		        </div>
+		    </div>
+		</section>
+	<script src="script/jQuery.js"></script>
+	<script src="library/bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 </html>
